1014acd1ba7366936f1635de1afe8d7650911774
Mahammad <mahammadjuberaus@gmail.com>
2023-08-27 01:22:20 +1000

    JS implemented for Public Pages

--- www/js/app.js
+++ www/js/app.js
@@ -7,146 +7,209 @@
  */
 var masterData = [{
     errorMessages: {
-        "email": "Please enter a valid email.",
-        "password": "Please enter a valid password.",
-        "mobile": "Please enter a valid mobile number.",
-        "firstName": "",
+        email: "Please enter a valid email.",
+        password: "Please enter a valid password.",
+        mobile: "Please enter a valid mobile number.",
+        firstName: "First name should be 3-50 characters.",
         /** TO-DO needs to be assigned with proper messages */
-        "lastName": "",
-        "cPassword": "",
-        "prefLocation": "",
-        "prefServiceCategories": "",
-        "notifPref": ""
-    }
-}]
+        lastName: "Last name should be 3-50 characters.",
+        cPassword: "Password and confirm password do not match",
+        prefLocation: "Please select a location.",
+        prefServiceCategories: "Please select atleast one option",
+        notifPref: "Please select atleast one option.",
+    },
+}, ];
 
 var baseURL = "http://localhost";
 var serverPort = 8000;
 var apiEndPoints = {
     login: "/login",
-    createVlntrAccount: "",
-    updateVlntrAccount: "",
-}
-
-
-
+    createVlntrAccount: "/dummy_url",
+    updateVlntrAccount: "/dummy_url",
+    //TO-DO Need to define all the endpoints
+};
+var routes = {
+    vlntrLogin: "",
+    vlntrHome: "",
+    vlntrNotif: "",
+    vlntrUpdateAcc: "",
+    //TO-DO Need to define all the routes
+};
 
 /**
  * JS Validation for all form inputs
- * @param {string} inputType 
- * @param {*} inputValue 
+ * @param {string} inputType
+ * @param {*} inputValue
  * @returns {boolean}
- * 
+ *
  */
 
-function isValidInput(inputType, inputValue) {
+function isValidInput(inputType, inputValue, originalPassword = "") {
     switch (inputType) {
         case "email":
-            // Using a regular expression to validate email format
-            if (/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(inputValue)) {
-                return true;
-            } else {
-                return false;
-            }
+            return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(inputValue);
+
         case "mobile":
-            // Using a regular expression to validate 10-digit mobile number
-            if (/^\d{10}$/.test(inputValue)) {
-                return true;
-            } else {
-                return false;
-            }
+            return /^\d{10}$/.test(inputValue);
+
         case "password":
-            if (inputValue.length >= 8) {
-                return true;
-            } else {
+            return inputValue.length >= 8; //TO-DO need to fix the length after discussion(8)
+
+        case "cPassword":
+            return originalPassword === inputValue;
+
+        case "firstName":
+            return /^[A-Za-z]{2,50}$/.test(inputValue); //TO-DO need to fix the length after discussion(Min:2,Max:50)
+
+        case "lastName":
+            return /^[A-Za-z]{2,50}$/.test(inputValue); //TO-DO need to fix the length after discussion(Min:2,Max:50)
+
+        case "prefLocation":
+        case "prefServiceCategories":
+        case "notifPref":
+            if (inputValue.trim() === "") {
+                //Empty means no option selected
                 return false;
+            } else {
+                return true;
             }
+
         default:
             console.log("Invalid input type");
             return false; // Invalid input type
     }
 }
 
-
-
 /**
  * To Display/Hide the error messages for all form validations
- * @param {string} inputType 
- * @param {boolean} isValid 
+ * @param {string} inputType
+ * @param {boolean} isValid
  */
 function setErrorMessage(inputType, isValid) {
-
     var errMsg = masterData[0]["errorMessages"][inputType]; //Custom message defined in masterdata
     var errorMessage = isValid ? "" : "<span>" + errMsg + "</span>";
     $(".err-msg[data-param='" + inputType + "']").html(errorMessage);
 }
 
-
 /**
  * Common function for AJAX request to the specified URL.
- * 
+ *
  * @param {string} url - The URL to send the request to.
  * @param {object} options - Additional options for the AJAX request.
  */
 
 function ajaxRequest(url, options) {
-
-
-    console.log(options);
+    var navigateTo = options.navigationUrl; //Empty if no need to redirect/navigate
 
     $.ajax({
         url: url,
-        type: options.method || 'GET', // or 'POST' if you're sending data
-        dataType: options.dataType || 'json', // the type of data you expect to receive
+        type: options.method || "GET", // or 'POST' if you're sending data
+        dataType: options.dataType || "json", // the type of data you expect to receive
         data: options.data || null, //Always be a JSON string
         success: function(data) {
-            // This function is called when the request is successful
-            $('#result').html(JSON.stringify(data));
-            //We can use successCallback function here
+            $("#result").html(JSON.stringify(data));
+            //TO-DO implement successCallback  here
+            if (navigateTo != "") {}
         },
         error: function(xhr, status, error) {
-            // This function is called when there's an error with the request
+            //TO-DO Proper error handling
             console.error(error);
-        }
+        },
     });
 } // END of common AJAX request function
 
-
 /**
- * 
- * @param {string} formId 
- * @returns {JSON} 
+ *
+ * @param {string} formId
+ * @returns {JSON}
  */
 
 function readFormData(formId) {
-
     var formData = $("#" + formId).serialize();
 
     // Deserialize the form data into an object
     var formDataObject = {};
-    formData.split('&').forEach(function(keyValue) {
-        var pair = keyValue.split('=');
-        formDataObject[pair[0]] = decodeURIComponent(pair[1] || '');
+    formData.split("&").forEach(function(keyValue) {
+        var pair = keyValue.split("=");
+        formDataObject[pair[0]] = decodeURIComponent(pair[1] || "");
     });
 
     formData = JSON.stringify(formDataObject);
 
     return formData;
-
 }
+
+/**
+ *
+ * @param {string} formId
+ */
+
+function validateForm(formId) {}
+
+/**
+ *
+ * @param {string} formId
+ * @returns {boolean}
+ */
+
+function finalFormValidation(formId) {
+    var allValidInputs = true; //deafult(boolean) :: if false form won't submit and shows error message at required fields
+    var isRequired = "false";
+    var allElements = $(
+        "#" + formId + " input, #" + formId + " select, #" + formId + " :radio"
+    );
+    var totalElements = allElements.length;
+    var processedElements = 0; //To wait for all iterations to complete
+
+    allElements.each(function() {
+        isRequired = $(this).data("required"); //predefined attr for every form element for validation{false if not req}
+        if (isRequired == true) {
+            inputType = $(this).data("param");
+            inputValue = $(this).val();
+            allValidInputs = isValidInput(inputType, inputValue);
+            if (allValidInputs == false) {
+                setErrorMessage(inputType, false); //Displays respective defined error message
+            } else {
+                setErrorMessage(inputType, true); //Erases if any prev error message
+            }
+        }
+        processedElements++; //Increase in number for each iteration
+    });
+    if (processedElements === totalElements) {
+        //Creates a wait to finish the .each();
+        return allValidInputs;
+    }
+} //END of final form validation :: common for entire application
 
 /**
  * 
  * @param {string} formId 
+ * @param {string} ajaxAPIURL 
  */
 
-function validateForm(formId) {
+function afterFormSubmission(formId, ajaxAPIURL, apiMethod) {
 
+    var isValidForm = finalFormValidation(formId);
+    if (isValidForm === true) {
+        //If all inputs are valid proceed to submission
+        var formData = readFormData(formId); //Returns a JSON string with form key&value pairs
+        var options = {
+            method: apiMethod,
+            data: formData,
+            navigateURL: "", //TO-DO Need to configure with predefined routes
+        };
+        //Req parameters are (URL : string,options: object with {method:'',datatype:'',data:JSON}
+        ajaxRequest(ajaxAPIURL, options);
+    } else {
+        //if any one of the required inputs are invalid show error message
 
-
+        $("html, body").animate({
+                scrollTop: $("#" + formId).offset().top,
+            },
+            1000
+        ); //Scroll to the top postion of the form to make user notice errors
+    }
 }
-
-
 
 /**
  * 
@@ -154,99 +217,53 @@ TO make sure all elements loaded to DOM
  */
 
 $(document).ready(function() {
-
     $('input[name="dates"]').daterangepicker();
 
-    //START of "login" authentication
-    $('#sign-in').click(function() {
-        var ajaxURL = apiEndPoints['login'];
+    //START of "create account"
+    $("#save-account").click(function() {
+        var ajaxURL = apiEndPoints["createVlntrAccount"];
         var formId = $(this).data("form-id");
-        var formData = readFormData(formId); //Returns a JSON string with form key&value pairs
+        afterFormSubmission(formId, ajaxURL, "POST"); //{*,*,[ANY ONE METHOD{POST/PUT/GET etc}]}Validates and performs api call
+    }); //END
 
-        var options = {
-                method: 'POST',
-                data: formData
-            }
-            //Req parameters are (URL : string,options: object with {method:'',datatype:'',data:JSON}
-        ajaxRequest(ajaxURL, options);
-
+    //START of "login" authentication
+    $("#sign-in").click(function() {
+        var ajaxURL = apiEndPoints["login"];
+        var formId = $(this).data("form-id");
+        afterFormSubmission(formId, ajaxURL, "POST");
     }); //END of "login" authentication
 
-
-    //START of "create account" 
-    $('#save-account').click(function() {
-
-        var ajaxURL = apiEndPoints['createVlntrAccount'];
-        var formId = $(this).data("form-id");
-        var formData = readFormData(formId); //Returns a JSON string with form key&value pairs
-
-        var options = {
-                method: 'POST',
-                data: formData
-            }
-            //Req parameters are (URL : string,options: object with {method:'',datatype:'',data:JSON}
-        ajaxRequest(ajaxURL, options);
-
-    }); //END of "create account" 
-
-
-    //START of "manage/update account" 
-    $('#update-account').click(function() {
-
-        var ajaxURL = apiEndPoints['updateVlntrAccount'];
-        var formId = $(this).data("form-id");
-        var formData = readFormData(formId); //Returns a JSON string with form key&value pairs
-
-        var options = {
-                method: 'POST',
-                data: formData
-            }
-            //Req parameters are (URL : string,options: object with {method:'',datatype:'',data:JSON}
-        ajaxRequest(ajaxURL, options);
-
-    }); //END of "manage/update account" 
-
-
-
-
+    //START of "manage/update account"
+    $("#update-account").click(function() {
+        var ajaxURL = apiEndPoints["updateVlntrAccount"];
+        afterFormSubmission(formId, ajaxURL, "PUT");
+    }); //END of "manage/update account"
 
 
 
     //START of "input" JS validation
-    $("input,select").blur(function() { //When user finish to input this event will be in action
-        console.log($(this).val());
+    $("input,select").blur(function() {
+        //When user finish to input this event will be in action
+
         // Get the current input element and its attributes
         var thisInputElem = $(this); // The input element being interacted with
         var thisInputVal = thisInputElem.val(); // Current value of the input
         var inputParam = thisInputElem.attr("data-param"); // Parameter associated with the input
+        var isEmpty = thisInputVal.trim() === "" ? true : false;
 
+        if (isEmpty == false) {
+            //IsEmptyCheck ::To avoid showing error message without submission or filling the input.
 
-        switch (inputParam) {
-            case "email":
-
-                // Check if the input value is a valid email
+            if (inputParam.length != 0 || inputParam != "") {
                 var isValid = isValidInput(inputParam, thisInputVal);
-                setErrorMessage(inputParam, isValid); //Displays/Hide an input error message
-                break;
-
-            case "mobile":
-                // Check if the input value is a valid password
-                var isValid = isValidInput(inputParam, thisInputVal); // Type and Value
-                setErrorMessage(inputParam, isValid); //Display/Hide an input error message
-
-                break;
-
-            case "password":
-
-                // Check if the input value is a valid password
-                var isValid = isValidInput(inputParam, thisInputVal); // Type and Value
-                setErrorMessage(inputParam, isValid); //Display/Hide an input error message
-
-            default:
-                console.log("Invalid data-param or empty.");
-                return false;
-
-
-        }
-    }); //END of input validation
-}); // END of document ready
+            } else if (inputParam === "cPassword") {
+                var originalPwdElemId = $(this).data("parent");
+                var originalPassword = $("#" + originalPwdElemId).val();
+                var isValid = isValidInput(inputParam, thisInputVal, originalPassword); //Validates input based on its type defined in attribute "data-param"
+            } else {
+                //TO-D0 throw error
+            }
+            setErrorMessage(inputParam, isValid); //Displays/Hide an input error message
+        } //END of IsEmptyCheck;
+    }); //END of input validation;
+}); // END of document ready;
--- www/js/index.js
+++ www/js/index.js
@@ -10,27 +10,27 @@ document.addEventListener('deviceready', onDeviceReady, false);
 function onDeviceReady() {
 
     $('#CreateNew').click(() => {
-		$("body").pagecontainer("change", "#createAcct");
+        $("body").pagecontainer("change", "#createAcct");
     });
 
     /* to be removed, just using while developing UI */
     $('#temp-button').click(() => {
-		$("body").pagecontainer("change", "#log-in");    
+        $("body").pagecontainer("change", "#log-in");
     });
     $('#temp-button-2').click(() => {
-      $("body").pagecontainer("change", "#manage-acct-page");    
-      });
+        $("body").pagecontainer("change", "#manage-acct-page");
+    });
     $('#temp-button-3').click(() => {
-       $("body").pagecontainer("change", "#log-#notif-page");    
-       });    
-    
+        $("body").pagecontainer("change", "#log-#notif-page");
+    });
+
     $('#save-account').click(() => {
-		$("body").pagecontainer("change", "#public-home"); 
+        //$("body").pagecontainer("change", "#public-home"); //TEMPORARY
     });
 
     /* footer navigation links */
 
-    
+
 
     console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
     document.getElementById('deviceready').classList.add('ready');

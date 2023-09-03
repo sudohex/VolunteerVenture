27054c61f320f07e77a689a127e1bc81804336da
Pyae Phyo Kyaw <sudohex@outlook.com>
2023-09-03 20:35:20 +1000

    fixed notification in server

--- server/routes/notifications.js
+++ server/routes/notifications.js
@@ -1,36 +1,41 @@
 const express = require("express");
 const router = express.Router();
 const Notification = require("../models/notification");
-const Staff = require("../models/staff");
 const auth = require("../middlware/auth");
 
-
-router.post("/", auth, async(req, res) => {
+router.post("/", auth, async (req, res) => {
     try {
         const { id } = req.body;
-
-        const notifications = await Notification.find({ sentTo: id });
-
-        // Collect all unique createdBy values from notifications
-        const createdByIds = notifications.map(notification => notification.createdBy);
-
-        // Fetch staff details 
-        const staffDetails = await Staff.find({ _id: { $in: createdByIds } });
-
-        // Create a map of createdBy IDs to staff names
-        const createdByMap = new Map(staffDetails.map(staff => [staff._id.toString(), `${staff.firstName} ${staff.lastName}`]));
-        console.log(createdByMap);
-        // Replacing createdBy values with staff names
-        const notificationsWithStaffNames = notifications.map(notification => ({...notification.toObject(),
-            createdBy: createdByMap.get(notification.createdBy.toString()) || notification.createdBy
-        }));
-
-        res.send(notificationsWithStaffNames);
+        // Fetch all notifications from the database
+        const allNotifications = await Notification.find();
+        // Filter out notifications where sentTo contains the id
+        const filteredNotifications = allNotifications.filter(
+            (notification) => notification.sentTo.includes(id)
+        );
+        res.send(filteredNotifications);
 
     } catch (err) {
         console.error(err.message);
         res.status(500).send("Server error");
     }
 });
+router.post("/create", auth, async (req, res) => {
+    try {
+        const { title, description, sentTo } = req.body;
+        const notification = new Notification({
+            title,
+            description,
+            sentTo,
+        });
+        await notification.save();
+        res.send(notification);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send("Server error");
+    }
+});
+
+
+
 
 module.exports = router;
--- server/routes/volunteers.js
+++ server/routes/volunteers.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const Volunteer = require("../models/volunteer"); // Make sure the path is correct
-const mongoose = require("mongoose");
 router.get("/", async (req, res) => {
   try {
    
